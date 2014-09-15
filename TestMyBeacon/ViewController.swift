96c6eb040a64f9cb1ac8920a3c728dfe1d35ae6b
Takahiro Masaki <tak.msk2580@gmail.com>
2014-09-15 22:40:10 +0900

    Initial Commit

---
+++ TestMyBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestMyBeacon
+//
+//  Created by Takahiro Masaki on 2014/09/15.
+//  Copyright (c) 2014年 Takahiro Masaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
